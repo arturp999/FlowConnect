88c79badd3a2f159fb80fff800745c24fba95c83
artur pereira <arturpe95@gmail.com>
2024-12-18 21:35:48 +0000

    initial setups add keycloak to pom added docker-compose to save in the project, changed both containers to run on the same network.

--- src/main/java/com/flow/connect/starter/FlowConnectApplication.java
+++ src/main/java/com/flow/connect/starter/FlowConnectApplication.java
@@ -1,4 +1,4 @@
-package com.flow.connect;
+package com.flow.connect.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
